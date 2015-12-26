ce607a8a37e874263d19a86f1cb99408defc60f0
Carlos Sanchez San Segundo <carlos.sanchez-sansegundo@prosegur.com>
2015-12-26 15:21:03 +0100

    Initial Commit

---
+++ Coursera_ProgramacionEnSwift_Semana4Tests/Coursera_ProgramacionEnSwift_Semana4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Coursera_ProgramacionEnSwift_Semana4Tests.swift
+//  Coursera_ProgramacionEnSwift_Semana4Tests
+//
+//  Created by Carlos Sanchez San Segundo on 26/12/15.
+//  Copyright © 2015 Carlos Sanchez San Segundo. All rights reserved.
+//
+
+import XCTest
+@testable import Coursera_ProgramacionEnSwift_Semana4
+
+class Coursera_ProgramacionEnSwift_Semana4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
